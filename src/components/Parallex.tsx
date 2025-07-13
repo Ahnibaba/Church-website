191cb19962ae9c8e2958a5ab637fbb2195a74404
Aniebiet Edet <Ahnibaba>
2025-07-13 14:53:00 +0100

    color

--- src/components/Parallex.tsx
+++ src/components/Parallex.tsx
@@ -2,7 +2,7 @@
 export const Parallax = () => {
   return (
     <div 
-      className="relative flex items-center justify-center h-[70vh]"
+      className="relative flex items-center justify-center w-full"
       style={{
         backgroundImage: "url('https://images.unsplash.com/photo-1543269865-cbf427effbad?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         backgroundPosition: "center",

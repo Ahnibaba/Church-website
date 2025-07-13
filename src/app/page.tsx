188a1272d9eedd73363db92af57722d0a32455cb
Aniebiet Edet <Ahnibaba>
2025-07-13 13:55:31 +0100

    next-event-prev/next button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <Slider />
         <NextEvent />
         <Welcoming />
-        <Parallax imageString='https://images.unsplash.com/photo-1543269865-cbf427effbad?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+        <Parallax />
      </>
    )
 }

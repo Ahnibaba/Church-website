fe493e185e0ea4f03d9115f50a6ff77b268f066d
Ahnibaba <aniebietedet2025@gmail.com>
2025-07-29 09:24:58 +0100

    writeup section

--- src/components/WriteupCard.tsx
+++ src/components/WriteupCard.tsx
@@ -9,13 +9,13 @@ type WriteupProps = {
 export default function WriteupCard({ writeupText, writeupTitle, writeupImage }: WriteupProps) {
   return (
     <section className="min-h-[90vh] p-5 sm:p-15">
-      <div className="w-full max-w-4xl mx-auto">
+      <div className="w-full max-w-4xl mx-auto my-5">
         <h1 className="font-light uppercase font-playFair text-2xl md:text-3xl lg:text-4xl tracking-wider text-[#d63037] text-center">
           {writeupTitle}
         </h1>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 justify-center items-center bg-[#D3CCBA]/20 my-5 sm:my-15 border-2 border-gray-300 overflow-hidden">
+      <div className="h-[70vh] grid grid-cols-1 lg:grid-cols-2 justify-center items-center bg-[#D3CCBA]/20 my-5 sm:my-15 border-2 border-gray-300 overflow-hidden">
         <div className="flex items-center justify-center p-5 sm:p-10 order-2 lg:order-1">
           <p className="text-gray-600 text-base sm:text-lg text-center sm:text-left font-roboto leading-relaxed">
             {writeupText}
--- src/components/nextEvent/NextEventData.ts
+++ src/components/nextEvent/NextEventData.ts
@@ -1124,8 +1124,7 @@ export const nextEventData: dataProps[] = [
     heroWriteUp: `
      GOD IS GOOD SERVICE – A Divine Experience You Can’t Afford to Miss!
      Join us for the "God is Good Service" — a power-packed encounter designed to refresh your spirit, ignite your faith, and leave you with a renewed sense of God’s goodness! This isn’t just another gathering; it’s a divine appointment with destiny.
-     Expect vibrant praise that will lift your heart, deep worship that ushers you into God’s presence, and spirit-filled ministrations that stir up your soul. The atmosphere will be alive with energy as we engage in soul-lifting concerts, transformative seminars, and powerful teachings from God’s Word that will leave you inspired and equipped for life.
-     Each session is uniquely designed to meet you at your point of need — whether you're longing for healing, direction, breakthrough, or just more of God. The "God is Good Service" is a celebration of His faithfulness, filled with joy, miracles, and undeniable testimonies.
+     
      
     `,
     testimony: [

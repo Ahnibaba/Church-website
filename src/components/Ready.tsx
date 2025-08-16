8ecebc009ee345037e08cdaee973aba127181177
unknown <Ahnibaba>
2025-08-16 14:26:12 +0100

    Are you ready

--- src/components/Ready.tsx
+++ src/components/Ready.tsx
@@ -3,7 +3,7 @@ export const Ready = () => {
         <section className="relative">
             {/* Background Image */}
             <div
-                className="w-full h-[50vh] md:h-[95vh] lg:h-[100vh]"
+                className="w-full h-[50vh] sm:h-[100vh] lg:h-[100vh]"
                 style={{
                     backgroundImage: `url("/ready.jpg")`,
                     backgroundSize: "cover",
@@ -18,7 +18,7 @@ export const Ready = () => {
                     <div className="grid grid-cols-2 gap-8">
                         {/* Text Section */}
                         <div className="flex items-center justify-center p-4">
-                            <h1 className="text-center font-roboto font-black text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-white">
+                            <h1 className="text-center font-roboto font-black text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-white [text-shadow:3px_4px_4px_rgba(255,255,255,0.8)] opacity-100">
                                 ARE YOU READY FOR THIS YEAR&apos;S EDITION
                             </h1>
                         </div>
@@ -27,7 +27,7 @@ export const Ready = () => {
                         <div className="relative h-64 sm:h-80 md:h-96 lg:h-[30rem] xl:h-[35rem]">
                             {/* First Image */}
                             <div
-                                className="absolute mt-5 left-0 h-40 w-40 sm:h-48 sm:w-48 md:h-56 md:w-56 lg:h-64 lg:w-64 xl:h-72 xl:w-72 2xl:h-80 2xl:w-80"
+                                className="absolute mt-5 left-0 sm:left-5 md:left-15 lg:left-20 xl:left-25 2xl:left-35 h-40 w-40 sm:h-48 sm:w-48 md:h-56 md:w-56 lg:h-64 lg:w-64 xl:h-72 xl:w-72 2xl:h-80 2xl:w-80"
                                 style={{
                                     backgroundImage: `url(/youth.jpg)`,
                                     backgroundSize: "cover",
@@ -38,7 +38,7 @@ export const Ready = () => {
                             
                             {/* Second Image */}
                             <div
-                                className="absolute left-20 sm:left-24 md:left-28 lg:left-32 xl:left-36 2xl:left-40 top-16 sm:top-20 md:top-24 h-32 w-32 sm:h-40 sm:w-40 md:h-48 md:w-48 lg:h-56 lg:w-56 xl:h-64 xl:w-64"
+                                className="absolute left-25 sm:left-35 md:left-45 lg:left-55 xl:left-65 2xl:left-75 top-16 sm:top-20 md:top-24 h-32 w-32 sm:h-40 sm:w-40 md:h-48 md:w-48 lg:h-56 lg:w-56 xl:h-64 xl:w-64"
                                 style={{
                                     backgroundImage: `url(/youth2.jpg)`,
                                     backgroundSize: "cover",
@@ -49,7 +49,7 @@ export const Ready = () => {
                             
                             {/* Third Image */}
                             <div
-                                className="absolute left-10 sm:left-12 md:left-16 lg:left-20 xl:left-24 2xl:left-28 top-32 sm:top-40 md:top-48 lg:top-56 xl:top-64 h-28 w-28 sm:h-36 sm:w-36 md:h-44 md:w-44 lg:h-52 lg:w-52 xl:h-60 xl:w-60"
+                                className="absolute left-10 sm:left-20 md:left-25 lg:left-35 xl:left-45 2xl:left-68 top-32 sm:top-40 md:top-48 lg:top-56 xl:top-64 h-28 w-28 sm:h-36 sm:w-36 md:h-44 md:w-44 lg:h-52 lg:w-52 xl:h-60 xl:w-60"
                                 style={{
                                     backgroundImage: `url(/youth3.jpg)`,
                                     backgroundSize: "cover",
